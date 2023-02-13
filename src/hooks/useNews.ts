a5eba2f70a53a34b921aa8243f67bdd0f31d122e
november-golf <n.s.gray@live.co.uk>
2023-02-13 16:04:24 +0000

    Changed useEffect dependency in useNews hook from SearchValue to SubmitSearch. Prevents API call firing on intitial render, and only when user searches by pressing enter or clicking the search button.

--- src/hooks/useNews.ts
+++ src/hooks/useNews.ts
@@ -1,23 +1,23 @@
 import {useEffect, useState} from 'react';
 
 
-
-
-export const useNews = (searchValue: string) => {
-    const [scoop, setScoop] = useState([{title:"Let's Search", description: "Text", name:"text"}]);
+export const useNews = (submitSearch: string) => {
+    const [scoop, setScoop] = useState([
+    { title: "Feeling curious?", description: "Search above and get some news!", name: "text" },
+  ]);
 
 
 useEffect(() => {
   async function getNews() {
     const apikey = process.env.REACT_APP_API_KEY;
-    const url = 'https://gnews.io/api/v4/search?q='+ searchValue + '&apikey=' + apikey + '&lang=en&country=us&max=10'
+    const url = 'https://gnews.io/api/v4/search?q='+ submitSearch + '&apikey=' + apikey + '&lang=en&country=us&max=10'
     const response = await fetch(url);
     const data = await response.json();
     setScoop(data.articles);
   
   }
-  getNews();
-}, [searchValue]);
+  if (submitSearch) getNews();
+}, [submitSearch]);
 
     return scoop;
 }
